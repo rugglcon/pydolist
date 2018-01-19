06b2619df5f4310ac8ab8b4c86ea48168c3a1ccf
Connor Ruggles <cruggles@iastate.edu>
2018-01-19 12:13:13 -0600

    version 0.4.1

--- pydo/config.py
+++ pydo/config.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 AUTHOR = "Connor Ruggles"
 EMAIL = "conruggles@gmail.com"
 URL = "https://github.com/rugglcon/pydolist"
